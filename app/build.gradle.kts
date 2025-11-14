147c33ce322b71d4c713a9bdb25404e0e78defc0
giacomomensio <giacomomensio@hotmail.it>
2025-11-14 18:42:10 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "com.giacomomensio.ricevapp"
+    compileSdk = 34
+
+    signingConfigs {
+        create("release") {
+            if (keystorePropertiesFile.exists()) {
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.giacomomensio.ricevapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation("androidx.security:security-crypto:1.0.0")
+    implementation("androidx.biometric:biometric:1.1.0")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/giacomomensio/ricevapp/MainActivity.kt
@@ -0,0 +1,324 @@
+package com.giacomomensio.ricevapp
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.webkit.CookieManager
+import android.webkit.JavascriptInterface
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private lateinit var saveCredentialsCheckbox: CheckBox
+    private lateinit var disclaimerContainer: ConstraintLayout
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private var justLoggedIn = false
+
+    private val USERNAME_KEY = "USERNAME_KEY"
+    private val PASSWORD_KEY = "PASSWORD_KEY"
+    private val PIN_KEY = "PIN_KEY"
+    private val SHOULD_SAVE_KEY = "SHOULD_SAVE_KEY"
+    private val DISCLAIMER_DISMISSED_KEY = "DISCLAIMER_DISMISSED_KEY"
+    private val HOME_PAGE_URL = "https://ivaservizi.agenziaentrate.gov.it/ser/documenticommercialionline/#/home"
+    private val NONAUTH_URL = "https://ivaservizi.agenziaentrate.gov.it/ser/documenticommercialionline/nonauth.html"
+    private val LOGIN_PAGE_URL = "https://ivaservizi.agenziaentrate.gov.it/portale/web/guest/home"
+    private val ALT_LOGIN_PAGE_URL = "https://ivaservizi.agenziaentrate.gov.it/portale/home"
+
+
+    inner class WebAppInterface {
+        @JavascriptInterface
+        fun onLoginButtonClick(username: String, password: String, pin: String) {
+            if (saveCredentialsCheckbox.isChecked) {
+                with(sharedPreferences.edit()) {
+                    putString(USERNAME_KEY, username)
+                    putString(PASSWORD_KEY, password)
+                    putString(PIN_KEY, pin)
+                    apply()
+                }
+            }
+        }
+    }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(false) {
+        override fun handleOnBackPressed() {
+            webView.goBack()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
+        webView = findViewById(R.id.webview)
+        saveCredentialsCheckbox = findViewById(R.id.save_credentials_checkbox)
+        disclaimerContainer = findViewById(R.id.disclaimer_container)
+
+        setupApp()
+
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState)
+        } else {
+            webView.visibility = View.INVISIBLE
+            saveCredentialsCheckbox.visibility = View.INVISIBLE
+            disclaimerContainer.visibility = View.INVISIBLE
+            authenticateApp()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
+    }
+
+    private fun authenticateApp() {
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> showBiometricPrompt()
+            else -> startApp()
+        }
+    }
+
+    private fun showBiometricPrompt() {
+        val executor = ContextCompat.getMainExecutor(this)
+        val biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    startApp()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Autenticazione fallita", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON || errorCode == BiometricPrompt.ERROR_USER_CANCELED) {
+                        finish()
+                    }
+                }
+            })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Autenticazione richiesta")
+            .setSubtitle("Sblocca per accedere all'app")
+            .setNegativeButtonText("Annulla")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun setupApp() {
+        webView.visibility = View.VISIBLE
+
+        CookieManager.getInstance().setAcceptCookie(true)
+
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        sharedPreferences = EncryptedSharedPreferences.create(
+            "secret_shared_prefs",
+            masterKeyAlias,
+            applicationContext,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        if (!sharedPreferences.getBoolean(DISCLAIMER_DISMISSED_KEY, false)) {
+            disclaimerContainer.visibility = View.VISIBLE
+            val disclaimerButton: Button = findViewById(R.id.disclaimer_button)
+            disclaimerButton.setOnClickListener {
+                disclaimerContainer.visibility = View.GONE
+                with(sharedPreferences.edit()) {
+                    putBoolean(DISCLAIMER_DISMISSED_KEY, true)
+                    apply()
+                }
+            }
+        } else {
+            disclaimerContainer.visibility = View.GONE
+        }
+
+        saveCredentialsCheckbox.isChecked = sharedPreferences.getBoolean(SHOULD_SAVE_KEY, false)
+
+        saveCredentialsCheckbox.setOnCheckedChangeListener { _, isChecked ->
+            with(sharedPreferences.edit()) {
+                putBoolean(SHOULD_SAVE_KEY, isChecked)
+                if (!isChecked) {
+                    remove(USERNAME_KEY)
+                    remove(PASSWORD_KEY)
+                    remove(PIN_KEY)
+                    Toast.makeText(this@MainActivity, "Salvataggio automatico disattivato", Toast.LENGTH_SHORT).show()
+                }
+                apply()
+            }
+        }
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.addJavascriptInterface(WebAppInterface(), "Android")
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsConfirm(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
+                AlertDialog.Builder(this@MainActivity)
+                    .setTitle("Conferma Uscita")
+                    .setMessage(message)
+                    .setPositiveButton("Sì") { _, _ ->
+                        result?.confirm()
+                        CookieManager.getInstance().removeAllCookies(null)
+                        CookieManager.getInstance().flush()
+                        view?.loadUrl("https://ivaservizi.agenziaentrate.gov.it/portale/logout")
+                    }
+                    .setNegativeButton("No") { _, _ -> result?.cancel() }
+                    .create()
+                    .show()
+                return true
+            }
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                onBackPressedCallback.isEnabled = view?.canGoBack() ?: false
+                if (url == NONAUTH_URL) {
+                    view?.loadUrl(LOGIN_PAGE_URL)
+                    return
+                }
+
+                val isLoginPageUrl = url == LOGIN_PAGE_URL || url == ALT_LOGIN_PAGE_URL
+
+                if (isLoginPageUrl) {
+                    view?.evaluateJavascript("""if (window.innerWidth < 768) { document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=768'); }""", null)
+                    saveCredentialsCheckbox.visibility = View.VISIBLE
+                    val jsLoginButtonListener = """ 
+                        (function() { 
+                            let doc = document; 
+                            const iframe = doc.getElementsByTagName('iframe')[0]; 
+                            if (iframe) { 
+                                try { doc = iframe.contentDocument; } catch(e) { return; } 
+                            } 
+                            const loginButton = doc.getElementById('login-button'); 
+                            if (loginButton) { 
+                                loginButton.addEventListener('click', function() { 
+                                    const uField = doc.getElementById('username'); 
+                                    const pField = doc.getElementById('password'); 
+                                    const pinField = doc.getElementById('pin'); 
+                                    if (uField && pField && pinField) { 
+                                        Android.onLoginButtonClick(uField.value, pField.value, pinField.value);
+                                    } 
+                                }); 
+                            } 
+                        })(); 
+                    """
+                    view?.evaluateJavascript(jsLoginButtonListener, null)
+                } else {
+                    view?.evaluateJavascript("""if (window.innerWidth < 600) { document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=600'); }""", null)
+                    saveCredentialsCheckbox.visibility = View.GONE
+                }
+
+                if (url != null && url.startsWith("https://ivaservizi.agenziaentrate.gov.it/")) {
+                    isLoginPage { onLoginPage ->
+                        if (onLoginPage) {
+                            justLoggedIn = true
+                            autofillCredentials(sharedPreferences)
+                        } else {
+                            if (justLoggedIn) {
+                                justLoggedIn = false
+                                if (url != HOME_PAGE_URL) {
+                                    view?.loadUrl(HOME_PAGE_URL)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun startApp() {
+        webView.visibility = View.VISIBLE
+        if (!sharedPreferences.getBoolean(DISCLAIMER_DISMISSED_KEY, false)) {
+            disclaimerContainer.visibility = View.VISIBLE
+        }
+        webView.loadUrl(HOME_PAGE_URL)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        CookieManager.getInstance().flush()
+    }
+
+    private fun isLoginPage(callback: (Boolean) -> Unit) {
+        val jsCheckLogin = """ 
+            (function() { 
+                let doc = document; 
+                let uField = doc.getElementById('username'); 
+                 if (!uField) { 
+                    const iframe = document.getElementsByTagName('iframe')[0]; 
+                    if (iframe) { 
+                         try { doc = iframe.contentDocument; } catch(e) { return false; } 
+                    } 
+                } 
+                return !!doc.getElementById('username'); 
+            })(); 
+        """ 
+        webView.evaluateJavascript(jsCheckLogin) { result ->
+            callback(result == "true")
+        }
+    }
+
+    private fun autofillCredentials(sharedPreferences: SharedPreferences) {
+        val savedUsername = sharedPreferences.getString(USERNAME_KEY, "") ?: ""
+        val savedPassword = sharedPreferences.getString(PASSWORD_KEY, "") ?: ""
+        val savedPin = sharedPreferences.getString(PIN_KEY, "") ?: ""
+
+        if (savedUsername.isNotBlank() && savedPassword.isNotBlank() && savedPin.isNotBlank()) {
+            val jsSetCredentials = """ 
+                (function() { 
+                    let doc = document; 
+                    let uField = doc.getElementById('username'); 
+                    if (!uField) {
+                        const iframe = document.getElementsByTagName('iframe')[0]; 
+                        if (iframe) { doc = iframe.contentDocument; } else { return; } 
+                    } 
+                    if(doc.getElementById('username')) { doc.getElementById('username').value = '$savedUsername'; } 
+                    if(doc.getElementById('password')) { doc.getElementById('password').value = '$savedPassword'; } 
+                    if(doc.getElementById('pin')) { doc.getElementById('pin').value = '$savedPin'; } 
+                })(); 
+            """ 
+            webView.post { webView.evaluateJavascript(jsSetCredentials, null) }
+        }
+    }
+}
