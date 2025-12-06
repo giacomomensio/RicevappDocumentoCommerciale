ff58930d68d89b86096bc345f7304ab6fc21ffb6
giacomomensio <giacomomensio@hotmail.it>
2025-12-06 13:03:03 +0100

    v1.3: Major Release - Extended compatibility SDK 23 & PIN Support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.giacomomensio.ricevapp"
         minSdk = 23
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.2.3"
+        versionCode = 11
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
